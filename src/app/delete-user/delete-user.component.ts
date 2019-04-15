44a05f2c871c16460a599fd5e6d0ca9989bf2a9f
Student <example@example.com>
2019-04-15 12:57:34 -0700

    Add code in component typescript files for Users, Restaurants and Orders

--- src/app/delete-user/delete-user.component.ts
+++ src/app/delete-user/delete-user.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { FirebaseObjectObservable } from 'angularfire2/database';
+
+import { User } from '../models/user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-delete-user',
   templateUrl: './delete-user.component.html',
-  styleUrls: ['./delete-user.component.css']
+  styleUrls: ['./delete-user.component.css'],
+  providers: [UserService]
 })
-export class DeleteUserComponent implements OnInit {
 
-  constructor() { }
+export class DeleteUserComponent implements OnInit {
+  userId: string;
+  userToDelete;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private userService: UserService) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.userId = urlParameters['id'];
+    });
+    this.userService.getUserById(this.userId).subscribe(dataLastEmittedFromObserver => {
+      this.userToDelete = dataLastEmittedFromObserver;
+    })
   }
 
+  goToShowUserPage(){
+    this.router.navigate(['users']);
+  }
+
+  onSelect(user: any) {
+    if (user.target.value === "true") {
+      this.userService.deleteUser(this.userToDelete);
+    }
+    this.goToShowUserPage();
+  }
 }
--- src/app/edit-order/edit-order.component.ts
+++ src/app/edit-order/edit-order.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { FirebaseObjectObservable } from 'angularfire2/database';
+
+import { Order } from '../models/order.model';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-edit-order',
   templateUrl: './edit-order.component.html',
-  styleUrls: ['./edit-order.component.css']
+  styleUrls: ['./edit-order.component.css'],
+  providers: [OrderService]
 })
+
 export class EditOrderComponent implements OnInit {
 
-  constructor() { }
+  orderId: string;
+  orderToUpdate;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private orderService: OrderService) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.orderId = urlParameters['id'];
+    });
+    this.orderService.getOrderById(this.orderId).subscribe(dataLastEmittedFromObserver => {
+      this.orderToUpdate = dataLastEmittedFromObserver;
+    })
   }
 
+  goToShowOrderPage() {
+      this.router.navigate(['orders']);
+  }
+
+  updateOrder(orderToUpdate) {
+    if (orderToUpdate.orderKey != "" && orderToUpdate.orderUserKey != "" && Date.parse(orderToUpdate.deliveryDateTime.toString()) != 0 && Date.parse(orderToUpdate.orderDateTime.toString()) != 0 && orderToUpdate.restaurantKey != "" && orderToUpdate.orderDetails.length > 0) {
+      this.orderService.updateOrder(orderToUpdate);
+      this.goToShowOrderPage();
+    } else {
+      alert('All fields are required!');
+    }
+  }
 }
