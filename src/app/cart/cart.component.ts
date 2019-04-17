043db9b5e8d6134245f46dec0c238216d3a05e80
Dave Sarbora <dsarbora@gmail.com>
2019-04-17 14:17:17 -0700

    show orders to shopping cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import { Restaurant } from '../models/restaurant.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
+  providers: [OrderService]
 })
 export class CartComponent implements OnInit {
-  order = null;
+  //order = null;
+  orders: FirebaseListObservable<any[]>;
 
   
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+    this.orders = this.orderService.getOrders();
   }
 
   orderSubmit() {
-    this.order = true;
+    // this.order = true;
   }
 
 }
