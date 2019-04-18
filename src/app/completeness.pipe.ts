5d56cec42cf4fcf6caf524fc8d9a508cfaf24e38
Dave Sarbora <dsarbora@gmail.com>
2019-04-17 18:15:26 -0700

    define output in completeness pipe

--- src/app/completeness.pipe.ts
+++ src/app/completeness.pipe.ts
@@ -8,7 +8,7 @@ import { Order } from './models/order.model';
 export class CompletenessPipe implements PipeTransform {
 
   transform(input: Order[]) {
-    var output: Order[];
+    let output: Order[] = [];
     for(let i=0; i<input.length; i++){
       if(input[i].status==="INCOMPLETED"){
         output.push(input[i]);
