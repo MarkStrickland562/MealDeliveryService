b755b444fd1f434ac8a3378214877fcf019c6204
Dave Sarbora <dsarbora@gmail.com>
2019-04-17 18:12:48 -0700

    add completeness pipe

---
+++ src/app/completeness.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CompletenessPipe } from './completeness.pipe';
+
+describe('CompletenessPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CompletenessPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
